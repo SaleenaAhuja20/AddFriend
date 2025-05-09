4eb94eb6e5da6c6ea9cf3498a8fd7e06b7460c3b
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-05-10 01:18:57 +0500

    js done

---
+++ script.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector(".btn");
+let text = document.querySelector(".text");
+
+let flag = "darkred";
+
+btn.addEventListener("click", ()=>{
+    if(flag === "darkred"){
+        text.style.color =  "rgb(63, 107, 189)";
+        btn.innerHTML = "cancel request";
+        btn.style.backgroundColor = "darkred";
+        text.textContent = "Friends";
+        flag = "blue";
+    }else{
+        text.style.color = "darkred";
+        btn.innerHTML = "Add Friend";
+        btn.style.backgroundColor = "rgb(63, 107, 189";
+        text.textContent = "Stranger";
+        flag = "darkred";
+    }
+});
